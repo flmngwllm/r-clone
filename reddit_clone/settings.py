fe76998ac8e1689eff59da9334e29dccd4a05fa0
Will <flmngwllm232@gmail.com>
2018-12-07 12:35:51 -0500

    added heroku

--- reddit_clone/settings.py
+++ reddit_clone/settings.py
@@ -14,6 +14,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+REACT_APP_DIR = os.path.join(BASE_DIR, 'client')
+
+STATICFILES_DIRS = [
+    os.path.join(REACT_APP_DIR, 'build', 'static')
+]
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,3 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import django_heroku
+django_heroku.settings(locals())
